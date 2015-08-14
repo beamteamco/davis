cc7a7413df6aa2835358bb2f599c360132626a34
Andrew Petersen <andy.p@comcast.net>
2015-08-14 13:08:39 -0600

    latest version

--- testing_script.m
+++ testing_script.m
@@ -1,20 +1,109 @@
 
-tilt = -28.6;
-% tilt = 0.5;
+try
+    close all
+    clear all
+end
+
+tiltPlane = -28.0315;
+inPlane   = -0.4975;
+inPlane   = -45;
 cx = 1024;
 cy = 1024;
 
- Rx =    [1,     0,               0;...
-         0,      cosd(tilt),    -sind(tilt);...
-         0,      sind(tilt),    cosd(tilt)];
+% Rx =    [1,     0,               0;...
+%          0,      cosd(tilt),    -sind(tilt);...
+%          0,      sind(tilt),    cosd(tilt)];
+   
+%Uses rodruiges rotation formula
+
+
+w = [cosd(90+tiltPlane);
+    sind(90+tiltPlane);
+    0];
+
+w_  =    [  0   -w(3)   w(2);
+            w(3)    0   -w(1);
+            -w(2)   w(1)    0];
         
-    
+I = [1 0 0;
+    0 1 0;
+    0 0 1];
+
+
+testThetas = 0:5:360;
+R_test = zeros(3,3,length(testThetas));
+
+for(i=1:length(testThetas))
+    R_test(:,:,i) = I + sind(testThetas(i))*w_ + (1-cosd(testThetas(i)))*(w_)^2;
+end
+
+R = I + sind(inPlane)*w_ + (1-cosd(inPlane))*(w_)^2;
+
+
+x = [1;0;0];
+
+% x_test = zeros(3,1,length(testThetas));
+% 
+% for(i=1:size(x_test,3))
+%     x_test(:,:,i) = R_test(:,:,i)*x;
+%     if(i==1)
+%         disp(R_test(:,:,i));
+%         disp(x);
+%         disp(R_test(:,:,i)*x);
+%     end
+% end
+
+x2 = R*x;
+
+figure        
+quiver3(0,0,0, w(1),w(2),w(3),'r');
+hold on
+quiver3(0,0,0, x(1),x(2),x(3),'b');
+quiver3(0,0,0, x2(1),x2(2),x2(3),'g');
+
+xlim([-1,1]);
+ylim([-1,1]);
+zlim([-1,1]);
 testImage = ReadInGE('C:\Users\Andy Petersen\Documents\MATLAB\Xray\2013_12_NiTi1Hf\Sample6_5_00006.tiff');
 
+
+pixelVectors = zeros(4,2048*2048);
+
+for(i=1:2048)
+    for(j=1:2048)
+         pixelVectors(:,2048*(i-1)+j) = [j-cx;-i+cy;0;testImage(i,j)];
+    end
+end
+
+pixelVectors_rotated = ((R*pixelVectors(1:3,:)).*([1;-1;1]*ones(1,size(pixelVectors,2)))) + ([cx;cy;0]*ones(1,size(pixelVectors,2)));
+pixelVectors_rotated = [pixelVectors_rotated;pixelVectors(4,:)];
+
+newIM = zeros(2048,2048);
+for(i=1:2048)
+    for(j=1:2048)        
+%         for(k=1:size(pixelVectors_rotated,2))                        
+%             if(sqrt(pixelVectors_rotated(1,k).^2+pixelVectors_rotated(2,k).^2) < ...
+%                     sqrt(pixelVectors_rotated(1,index).^2+pixelVectors_rotated(2,index).^2))
+%                 index((2048*(i-1)+ j))=k;
+%             end
+%         end
+        if(ceil(pixelVectors_rotated(1,2048*(i-1)+j)) > 0 && ceil(pixelVectors_rotated(1,2048*(i-1)+j)) < 2049 &&...
+                ceil(pixelVectors_rotated(2,2048*(i-1)+j)) > 0 && ceil(pixelVectors_rotated(2,2048*(i-1)+j)) < 2049)
+            newIM(ceil(pixelVectors_rotated(2,2048*(i-1)+j)),ceil(pixelVectors_rotated(1,2048*(i-1)+j))) = pixelVectors_rotated(4,2048*(i-1)+j);
+        end
+    end
+end
+disp('done')
+
 figure
-imagesc(testImage,[0,max(max(testImage))]);
+imagesc(testImage,[0,max(max(testImage))]);  
+title('Original');
 axis square
-%tilt only
+
+figure
+imagesc(newIM,[0,max(max(testImage))]);  
+title('Adjusted');
+axis square
 
 
 
@@ -39,20 +128,20 @@ axis square
 % xx2=reshape(imVectors2,2048,2048,3);
 % figure
 % mesh(xx2(:,:,3));
-newIm = zeros(4096,4096);
-c=sind(90+tilt);
-for(i=1:size(testImage,2))
-    for(j=1:size(testImage,1))
-        ny = (cy-j)/c;        
-        npy = -(ny-1024);
-        newIm(ceil(npy)+1024,i+1024) = testImage(j,i); %note 1024 in here is padding
-    end
-end
-
-figure
-imagesc(newIm,[0,max(max(newIm))]);
-axis square
-disp('done')
+% % % newIm = zeros(4096,4096);
+% % % c=sind(90+tilt);
+% % % for(i=1:size(testImage,2))
+% % %     for(j=1:size(testImage,1))
+% % %         ny = (cy-j)/c;        
+% % %         npy = -(ny-1024);
+% % %         newIm(ceil(npy)+1024,i+1024) = testImage(j,i); %note 1024 in here is padding
+% % %     end
+% % % end
+% % % 
+% % % figure
+% % % imagesc(newIm,[0,max(max(newIm))]);
+% % % axis square
+% % % disp('done')
 
 % % th = 4.7032;
 % % eta = 0:360/72:360-360/72;
