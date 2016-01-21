72112b44ae6b80cdf55d48f50cf3f7532267eafa
apetersen0 <andy.p@comcast.net>
2016-01-20 17:09:46 -0700

    Added 2nd calculation method

--- maxOverAllImages.m
+++ maxOverAllImages.m
@@ -3,20 +3,27 @@ function output = maxOverAllImages(images)
 n = length(images);
 temp = zeros(2048,2048);
 
-h = waitbar(0,'Computing Max Over All Images');
-for(i=1:n-1)
-    if(i == 1)
-        k = floor(images{i+1}./images{i});
-        k = k./k;
-        k(isnan(k))=0;
-        temp = k.*images{i+1} + (k*-1 + 1).*images{i};
-    else
-        k = floor(images{i+1}./temp);
-        k = k./k;
-        k(isnan(k))=0;
-        temp = k.*images{i+1} + (k*-1 + 1).*temp;
-    end  
-    waitbar(i/(n-1),h,'Computing Max Over All Images');
+%% Method 1
+% h = waitbar(0,'Computing Max Over All Images');
+% for(i=1:n-1)
+%     if(i == 1)
+%         k = floor(images{i+1}./images{i});
+%         k = k./k;
+%         k(isnan(k))=0;
+%         temp = k.*images{i+1} + (k*-1 + 1).*images{i};
+%     else
+%         k = floor(images{i+1}./temp);
+%         k = k./k;
+%         k(isnan(k))=0;
+%         temp = k.*images{i+1} + (k*-1 + 1).*temp;
+%     end  
+%     waitbar(i/(n-1),h,'Computing Max Over All Images');
+% end
+% close(h);
+% output = temp;
+
+%% Method 2
+for(i=1:n)
+    temp = max(temp,images{i});
 end
-close(h);
 output = temp;
